a4f2615b8481f1eb73fa9124b3739b6b9e8e9b1f
w495833056 <yangmei@yunkan-2.local>
2015-03-12 16:07:25 +0800

    Initial Commit

---
+++ YKloveEdu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YKloveEdu
+//
+//  Created by yangmei on 15/3/12.
+//  Copyright (c) 2015年 yangmei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
